08170df7e1d2d61d6de1b16e42e19fb239023765
Florin Florinescu <florinutz@gmail.com>
2012-03-06 00:31:35 +0200

    torrentz 1

--- lib/movie.rb
+++ lib/movie.rb
@@ -1,9 +1,81 @@
 module Steal
   
-  class TorrentzItem
+  TESTING = true
   
-  class Movie < TorrentzItem
-    require 'imdb'
+  class TItem
+   
+    attr_accessor :hash, :page, :sources
+    
+    BASE_URL = 'http://torrentz.eu/'
+    
+    def initialize hash
+      @hash, @sources = hash, []
+    end
+    
+    def url
+      BASE_URL + @hash
+    end
+    
+    def fetchSources
+      require 'rubygems'
+      require 'open-uri'
+      require 'nokogiri'
+      if TESTING
+        from = File.open('/home/florin/workspace/torz/cache/0581dbd0e8917658ed5b641d3b037002cbe5965c.html')
+      else 
+        from = open(url)
+      end
+      @page = Nokogiri::HTML(from)
+      links = @page.css('div.download dl dt a')
+      links[1..links.count].each do |link|
+        href = link["href"]
+        name = link.css('span').first.content
+        @sources << TSource.new(self, 'name', href, 'css')
+      end
+    end
+    
+    def inspect
+      "TItem object with url #{BASE_URL + @hash}"
+    end
+    
+  end
+  
+  
+  class TSource
+
+    attr_accessor :item, :url, :css, :name
+
+    def initialize item, name, url, css
+      @item, @name, @url, @css = item, name, url, css
+    end
+
+    def self.getFile url, css, saveTo
+      
+    end
+
+    def self.download full_url, to_here
+      require 'open-uri'
+      writeOut = open(to_here, "wb")
+      writeOut.write(open(full_url).read)
+      writeOut.close
+    end
+     
+    def inspect
+      "\nTSOURCE<#@name-#@url>"
+    end 
+    
+  end
+  
+  
+  class TConfig
+    require 'yaml'
+    def initialize file='../config/sources.yml'
+      yml = YAML.load_file(file);
+    end
+  end
+  
+  
+  class Movie < TItem
     attr_accessor :imdb
     def initialize()
     end
--- main.rb
+++ main.rb
@@ -1,6 +1,12 @@
 #!/usr/bin/env ruby
 $LOAD_PATH << './lib'
+require 'rubygems'
+require 'open-uri'
+require 'nokogiri'
+require 'yaml'
+require 'imdb'
 require 'movie'
 
-movie = Steal::Movie.new
-puts movie.search
+item = Steal::TItem.new('0581dbd0e8917658ed5b641d3b037002cbe5965c')
+item.fetchSources
+puts item.sources.inspect
